d8c7b991f9c51c8fb4600703257b7018e05cc4c8
seung-jae-choi <54825536+seung-jae-choi@users.noreply.github.com>
2020-01-02 02:01:25 +0900

    삼성문제

--- pratice 1.cpp
+++ pratice 1.cpp
@@ -1,28 +1,92 @@
-﻿#include<iostream>
+﻿/*
+#include<iostream>
 using namespace std;
 
 int main()
 {
-	int  kg;
-	int count = 0;
-	cin >> kg;
-	while (kg > 0)
+	int T;
+	cin >> T;
+	int* profit;
+	profit = new int[T];
+	int N;
+	int* temp = NULL;
+	int maxIndex;
+	int i, j, k;
+
+	for (i = 0; i < T; i++)
 	{
-		if (kg % 5 == 0)
+		maxIndex = 0;
+		profit[i] = 0;
+		cin >> N;
+		if (temp == NULL)
 		{
-			count += (kg / 5);
-			kg %= 5;
+			temp = new int[N];
 		}
-		else if (kg >= 3)
+		else if ((sizeof(*temp) / sizeof(int)) < N)
 		{
-			kg -= 3;
-			count++;
+			delete[] temp;
+			temp = new int[N];
 		}
-		else
+		for (j = 0; j < N; j++)
 		{
-			count = -1;
-			break;
+			cin >> temp[j];
+			if (temp[j] > temp[maxIndex]) maxIndex = j;
+		}
+		j = 0;
+		while (maxIndex < N)
+		{
+			for (j; j < maxIndex; j++)
+			{
+				profit[i] += (temp[maxIndex] - temp[j]);
+			}// j = maxIndex;
+			j++;
+			maxIndex = j;
+			for (k = j; k < N; k++)
+			{
+				if (temp[k] > temp[maxIndex]) maxIndex = k;
+			}
 		}
 	}
-	cout << count;
+	for (i = 0; i < T; i++)
+	{
+		cout << "#" << i+1 << " " << profit[i]<<endl;
+	}
+	delete[] temp;
+	delete[] profit;
+}
+*/
+#include<iostream>
+using namespace std;
+#define MAX 1000000
+int temp[MAX];
+int main()
+{
+	int T;
+	cin >> T;
+	long long profit;
+	int N;
+
+	int maxIndex;
+	int i, j;
+
+	for (i = 0; i < T; i++)
+	{
+		profit = 0;
+		cin >> N;
+
+		for (j = 0; j < N; j++)
+		{
+			cin >> temp[j];
+		} // j == N;
+		j--;
+		maxIndex = N - 1;
+		for (j; j >= 0; j--)
+		{
+			if (temp[maxIndex] > temp[j]) 
+				profit += (temp[maxIndex] - temp[j]);
+			else
+				maxIndex = j;
+		}
+		cout << "#" << i + 1 << " " << profit << endl;
+	}
 }
