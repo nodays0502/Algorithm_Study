131190cfcf8416b9d12f8670fa6de62633f72e48
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-03-26 17:26:34 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_14502.java
+++ JAVA/BOJ/BOJ_14502.java
@@ -1,72 +1,73 @@
-package study;
+package Lecture;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
+// 2시 40분
 public class BOJ_14502 {
-	static int n,m;
+	static int n , m ;
 	static int input[][];
-	static int count;
-	static int dx[] = {0,1,0,-1};
-	static int dy[] = {1,0,-1,0};
-	static int result = 0 ;
-	static void comb(int cnt, int start) {
-		if(cnt == 3) {
-			bfs();
-		}else {
-			int i = start / m;
-			for(int j = start % m ; j < m ; j++) {
+	static int safeCnt = 0;
+	static int dy[] = {0,0,1,-1};
+	static int dx[] = {1,-1,0,0};
+	static int result = 0;
+	static List<int[]> virus = new ArrayList<>();
+
+	static void comb(int depth, int start) {
+//		System.out.println("comb");
+		if(depth == 0) {
+			int virus = bfs() + 3; // 0을 3개 1로 변했기 때문에 0의 개수를 이만큼 빼준다.
+			result = Math.max(result, safeCnt - virus);
+			return ;
+		}
+		int i = start / m;
+		for(int j = start % m ; j < m ; j++) {
+			if(input[i][j] == 0) {
+				input[i][j] = 1;
+				comb(depth-1 , i * m + j);
+				input[i][j] = 0;
+			}
+		}
+		for(i = (start / m) + 1 ; i < n ; i++) {
+			for(int j = 0 ; j < m ; j++) {
 				if(input[i][j] == 0) {
 					input[i][j] = 1;
-					comb(cnt+1 , i * m + j);
+					comb(depth-1 , i * m + j);
 					input[i][j] = 0;
-				}
-			}
-			for(i = (start / m) + 1 ; i < n ; i++) {
-				for(int j = 0 ; j < m ; j++) {
-					if(input[i][j] == 0) {
-						input[i][j] = 1;
-						comb(cnt+1 , i * m + j);
-						input[i][j] = 0;
-					}
 				}
 			}
 		}
 	}
-	static void bfs() {
-		boolean check[][] = new boolean[n][m];
-	//	System.out.println("bfs()");
-		int sum = 0 ;
+	static int bfs() {
+//		System.out.println("bfs");
+		boolean visited[][] = new boolean[n][m];
 		Queue<int[]> q = new LinkedList<>();
-		for(int i = 0 ; i < n ; i++) {
-			for(int j = 0 ; j < m ; j++) {
-				if(input[i][j] == 2) {
-					q.offer(new int[] {i,j});
-					check[i][j] = true;
-	//				sum++;
-				}
-			}
+		for(int i = 0 ; i < virus.size(); i++) {
+			int y = virus.get(i)[0];
+			int x = virus.get(i)[1];
+			q.offer(new int[] {y,x});
 		}
-//		System.out.println(sum);
+		int result = 0 ;
 		while(!q.isEmpty()) {
 			int[] now = q.poll();
-//			System.out.println(now[0] +" "+now[1]);
-			for(int i = 0 ; i < 4; i++) {
-				int ny = now[0] + dy[i];
-				int nx = now[1] + dx[i];
-				if(nx >= 0 && nx < m && ny >= 0 && ny < n && input[ny][nx] == 0 && check[ny][nx] == false) {
-					check[ny][nx] = true;
-					q.offer(new int[] {ny,nx});
-					sum++;
+			int y = now[0];
+			int x = now[1];
+			for(int i = 0 ; i < 4 ; i++) {
+				int ny = y + dy[i]; 
+				int nx = x + dx[i];
+				if(nx >= 0 && nx < m && ny >= 0 && ny < n && input[ny][nx] == 0 && !visited[ny][nx]) {
+					result++;
+					visited[ny][nx] = true;
+					q.offer(new int[] {ny, nx});
 				}
 			}
 		}
-		result = Math.max(result, count - sum - 3);
+		return result;
 	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -78,11 +79,13 @@ public class BOJ_14502 {
 			st = new StringTokenizer(br.readLine()," ");
 			for(int j = 0 ; j < m ; j++) {
 				input[i][j] = Integer.parseInt(st.nextToken());
-				if(input[i][j] == 0) count++;
+				if(input[i][j] == 0 ) safeCnt++;
+				else if(input[i][j]  == 2) {
+					virus.add(new int[] {i,j});
+				}
 			}
 		}
-//		System.out.println(count);
-		comb(0,0);
+		comb(3,0);
 		System.out.println(result);
 	}
 }
