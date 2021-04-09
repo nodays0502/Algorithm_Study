b0b20927ca168723e11f6a3794a27e42702455a1
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-04-09 17:46:23 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_2003.java
+++ JAVA/BOJ/BOJ_2003.java
@@ -1,4 +1,4 @@
-package SsafyPrac;
+package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -7,34 +7,34 @@ import java.util.StringTokenizer;
 public class BOJ_2003 {
 	static int N, M;
 	static int input[];
-	static int result = 0 ;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		input = new int[N];
 		st = new StringTokenizer(br.readLine()," ");
+		input = new int[N];
 		for(int i = 0 ; i < N ; i++) {
 			input[i] = Integer.parseInt(st.nextToken());
 		}
-		int sp = 0;
-		int ep = 0;
-		int sum = 0 ;
+		int si = 0;
+		int ei = 0;
+		int sum = 0;
+		int result = 0 ;
 		while(true) {
 			if(sum < M) {
-				if(ep >= N )break;
-				sum += input[ep++];
-			}
-			else if(sum == M) {
-				result++;
-				sum -= input[sp++];
+				if(ei >= N) {
+					break;
+				}
+				sum += input[ei++];
 			}else {
-				sum -= input[sp++];
+				if(sum == M) {
+					result++;
+				}
+				sum -= input[si++];
+				if(si >= N) break;
 			}
-			if(sp >= N) break;
 		}
 		System.out.println(result);
-		
 	}
 }
