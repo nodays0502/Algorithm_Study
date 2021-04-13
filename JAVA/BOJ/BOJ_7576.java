bd13cee15741be6a5b83b2ef7c7d46070978882e
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-04-14 00:17:53 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_7576.java
+++ JAVA/BOJ/BOJ_7576.java
@@ -1,4 +1,4 @@
-package BFS;
+package Lecture0413;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -7,40 +7,43 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class BOJ_7576 {
-	static int N,M;
-	static int input[][];
-	static int dp[][];
-	static int dy[] = {0,1,0,-1};
-	static int dx[] = {1,0,-1,0};
-	static int result = 0;
-	static void bfs() {
+	static int N , M;
+	static int map[][];
+	static int dy[] = {0,0,1,-1};
+	static int dx[] = {1,-1,0,0};
+	static int bfs() {
+		int result = -1 ;
 		Queue<int[]> q = new LinkedList<>();
 		for(int i = 0 ; i < N ; i++) {
-			for(int j = 0 ; j < M ; j++) {
-				if(input[i][j] == 1) q.offer(new int[] {i,j});
-				else{dp[i][j] = -1;}
-			}
-		}
-		while(!q.isEmpty()) {
-	//		System.out.println(q);
-			int[] now = q.poll();
-			for(int i =  0 ; i < 4 ; i++) {
-				int ny = now[0] + dy[i];
-				int nx = now[1] + dx[i];
-				int count = dp[now[0]][now[1]];
-				if(nx >=0 && nx < M && ny >=0 && ny < N && dp[ny][nx] == -1 && input[ny][nx] == 0) {
-					input[ny][nx] = 1;
-					dp[ny][nx] = count + 1;
-					q.offer(new int[] {ny,nx});
-					result = count+1;
+			for(int j = 0 ;  j< M ; j++) {	
+				if(map[i][j] == 1) {
+					q.add(new int[] {i,j});
 				}
 			}
 		}
+		int cnt = 0 ;
+		while(!q.isEmpty()) {
+			int size = q.size();
+			if(check()) {result = cnt; break;}
+			for(int t = 0 ;t < size; t++) {
+				int[] now  = q.poll();
+				for(int i = 0 ; i < 4; i++) {
+					int ny = now[0] + dy[i];
+					int nx = now[1] + dx[i];
+					if(nx>=0 && nx < M && ny >=0 && ny < N && map[ny][nx] == 0) {
+						map[ny][nx] = 1;
+						q.offer(new int[] {ny,nx});
+					}
+				}
+			}
+			cnt++;
+		}
+		return result;
 	}
 	static boolean check() {
 		for(int i = 0 ; i < N ; i++) {
-			for(int j = 0 ; j < M ; j++) {
-				if(input[i][j] == 0) return false;
+			for(int j = 0 ;  j< M ; j++) {
+				if(map[i][j] == 0 ) return false;
 			}
 		}
 		return true;
@@ -50,16 +53,15 @@ public class BOJ_7576 {
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
-		dp = new int[N][M];
-		input = new int[N][M];
+		map = new int[N][M];
 		for(int i = 0 ; i < N ; i++) {
-			 st = new StringTokenizer(br.readLine()," ");
-			for(int j = 0 ; j < M ; j++) {
-				input[i][j] = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine()," ");
+			for(int j = 0 ;  j < M ; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		bfs();
-		if(check())System.out.println(result);
-		else System.out.println(-1);
+	//	System.out.println("?");
+		System.out.println(bfs());
 	}
+
 }
