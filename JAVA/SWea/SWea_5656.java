0fd9ae6ea8482f4c81899be1db2d822523f5b1fc
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-04-14 21:06:26 +0900

    Add files via upload

--- JAVA/SWea/SWea_5656.java
+++ JAVA/SWea/SWea_5656.java
@@ -1,125 +1,114 @@
-package Study;
+package HW;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class SWea_5656 {
-	static int N , W , H;
-	static int input[][];
-	static int result = Integer.MAX_VALUE; 
-	static int dy[] = {0,1,0,-1};
-	static int dx[] = {1,0,-1,0};
-	static void count(int[][] arr) {
-		int cnt = 0 ;
-		for(int i = 0 ; i < H ; i++) {
-			for(int j = 0 ; j < W ; j++) {
-				if(arr[i][j] > 0) cnt++;
+	static int n,m,k;
+	static int map[][];
+	static int dy[] = {-1,0,1,0};
+	static int dx[] = {0,1,0,-1};
+	static int result = Integer.MAX_VALUE;
+	static void gravity(int[][] arr) {
+		for(int j  = 0 ; j< m ; j++) {
+			Queue<Integer> q = new LinkedList<>();
+			for(int i = n-1 ; i >= 0 ; i--) {
+				if(arr[i][j] != 0) {
+					q.offer(arr[i][j]);
+					arr[i][j] = 0;
+				}
+			}
+			for(int i = n-1 ; i >= 0 ; i--) {
+				if(!q.isEmpty()) {
+					arr[i][j] = q.poll();
+				}else {
+					break;
+				}
+			}
+		}
+	}
+	static void counting(int[][]arr) {
+		int cnt = 0;
+		for(int i = 0 ; i < n ; i++) {
+			for(int j = 0 ; j < m; j++) {
+				if(arr[i][j] != 0) {
+					cnt++;
+				}
 			}
 		}
 		result = Math.min(result, cnt);
 	}
-	
-	static void swap(int[][]arr, int y,int x , int ny , int nx) {
-		int temp = arr[y][x];
-		arr[y][x] = arr[ny][nx];
-		arr[ny][nx] = temp;
-	}
-	
-	static void down(int[][] arr) {
-		for(int i = 0 ; i < W ; i++) {
-			int bottomIndex = H-1;
-			for(int j = H-1 ; j >= 0 ; j--) {
+	static void dfs(int depth,int[][] arr) {
+		if(depth == k) {
+			counting(arr);
+			return ;
+		}
+		boolean find = false;
+		for(int i = 0 ; i < m ; i++) {
+			for(int j = 0 ; j < n; j++) {
 				if(arr[j][i] != 0) {
-					swap(arr,j,i,bottomIndex,i);
-					bottomIndex--;
+					find = true;
+					int[][] temp = makeTemp(arr);
+					pung(j,i,temp);
+					gravity(temp);
+					dfs(depth+1,temp);
+					break;
 				}
 			}
 		}
+		if(!find) 
+			counting(arr);
 	}
-	
-	static void copy(int[][] arr1 , int[][] arr2) { // depp copy arr1 = arr2;
-		for(int i = 0 ; i < H; i++) {
-			for(int j = 0 ; j < W; j++) {
-				arr1[i][j] = arr2[i][j];
+	private static void pung(int y, int x, int[][] temp) {
+		if(map[y][x] == 0) return;
+		int length = temp[y][x];
+		temp[y][x] = 0;
+		for(int i = 0 ; i < 4; i++) {
+			int ny = y;
+			int nx = x;
+			for(int j = 1; j < length ; j++) {
+				ny += dy[i];
+				nx += dx[i];
+				if(nx >= 0 && nx < m && ny >=0 && ny < n)
+					pung(ny,nx,temp);
+				else 
+					continue;
 			}
 		}
+		
 	}
-	
-	static void perm(int depth, int [][] arr) {
-	//	print(arr);
-		if(depth == N) {
-			count(arr);
-		}else {
-			boolean find = false;
-			for(int i = 0 ; i < W ; i++) {
-				int[][] temp = new int[H][W];
-				copy(temp , arr);
-				for(int j = 0 ; j < H ; j++) {
-					if(arr[j][i] != 0 ) {
-						find = true;
-				//		System.out.println("전");
-				//		print(temp);
-						crush(temp,j,i);
-				//		System.out.println("후");
-				//		print(temp);
-						down(temp);
-						perm(depth+1,temp);
-						break;
-					}
-				}
-			}
-			if(!find) result = 0 ;
-		}
-	}
-	
-	static void crush(int[][] arr , int y , int x) {
-		int length = arr[y][x] - 1;
-		arr[y][x] = 0;
-		for(int i = 0 ; i < 4 ; i++) {
-			for(int j = 1 ; j <= length ; j++) {
-				int ny = y + j * dy[i];
-				int nx = x + j * dx[i];
-				if(nx >= 0 && nx < W && ny >= 0 && ny < H) {
-					if(arr[ny][nx] == 1)
-						arr[ny][nx] = 0;
-					if(arr[ny][nx] != 0 && arr[ny][nx] != 1) { 
-						crush(arr,ny,nx);
-					}
-				}
+	static int[][] makeTemp(int[][] arr){
+		int[][] temp = new int[n][m];
+		for(int i = 0 ; i < n ; i++) {
+			for(int j = 0 ; j < m ; j++) {
+				temp[i][j] = arr[i][j];
 			}
 		}
-	}
-	static void print(int[][] arr) {
-		for(int i = 0 ; i < H ; i++) {
-			for(int j = 0 ; j < W ; j++) {
-				System.out.print(arr[i][j]+" ");
-			}
-			System.out.println();
-		}
-		System.out.println();
+		return temp;
 	}
 	public static void main(String[] args) throws Exception {
-		System.setIn(new FileInputStream("src/SWea_5656.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		int T = Integer.parseInt(st.nextToken());
-		for(int t = 1 ; t <= T ; t++) {
-			st = new StringTokenizer(br.readLine()," ");
-			N = Integer.parseInt(st.nextToken());
-			W = Integer.parseInt(st.nextToken());
-			H = Integer.parseInt(st.nextToken());
+		for(int t= 1; t <= T ; t++) {
 			result = Integer.MAX_VALUE;
-			input = new int [H][W];
-			for(int i = 0 ; i < H ; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			k = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			n = Integer.parseInt(st.nextToken());
+			map = new int[n][m];
+			for(int i = 0 ; i < n ; i++) {
 				st = new StringTokenizer(br.readLine()," ");
-				for(int j = 0 ; j < W ; j++) {
-					input[i][j] = Integer.parseInt(st.nextToken());
+				for(int j = 0 ; j < m ; j++) {
+					map[i][j]= Integer.parseInt(st.nextToken());
 				}
 			}
-			perm(0,input);
-			System.out.println("#"+t+" "+result);
+			dfs(0,map);
+			System.out.printf("#%d %d%n",t,result);
 		}
 	}
+
 }
