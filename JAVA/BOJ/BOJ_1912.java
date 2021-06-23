e52a98326755cfb04b42f1df0ea14eb7b0717a4f
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-06-23 11:50:24 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_1912.java
+++ JAVA/BOJ/BOJ_1912.java
@@ -1,27 +1,27 @@
-package BOJ;
+package BruteForce;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_1912 {
+	static int n;
+	static int input[];
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		int n = Integer.parseInt(st.nextToken());
-		int[] input = new int[n];
-		int[] dp = new int[n];
-		st = new StringTokenizer(br.readLine()," ");
-		for(int i = 0 ; i < n ; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-		}
-		int result = -10000;
+		n = Integer.parseInt(st.nextToken());
 		int sum = 0;
+		int result = Integer.MIN_VALUE;
+		st = new StringTokenizer(br.readLine()," ");
+		input = new int[n];
 		for(int i = 0 ; i < n ; i++) {
+			input[i] = Integer.parseInt(st.nextToken()); 
 			sum += input[i];
-			dp[i] = sum;
+			if(sum < 0) {
+				sum = 0 ;
+			}
 			result = Math.max(result, sum);
-			if(sum < 0) sum = 0;
 		}
 		System.out.println(result);
 	}
