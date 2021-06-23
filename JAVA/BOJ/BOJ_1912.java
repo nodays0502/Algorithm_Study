bc59e8494e33c4ed734baf0090e1dfb0f93f55c0
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-06-23 11:51:02 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_1912.java
+++ JAVA/BOJ/BOJ_1912.java
@@ -12,16 +12,16 @@ public class BOJ_1912 {
 		StringTokenizer st = new StringTokenizer(br.readLine()," ");
 		n = Integer.parseInt(st.nextToken());
 		int sum = 0;
-		int result = Integer.MIN_VALUE;
+		int result = -10001;
 		st = new StringTokenizer(br.readLine()," ");
 		input = new int[n];
 		for(int i = 0 ; i < n ; i++) {
 			input[i] = Integer.parseInt(st.nextToken()); 
 			sum += input[i];
-			if(sum < 0) {
-				sum = 0 ;
-			}
 			result = Math.max(result, sum);
+			if(sum < 0) {
+				sum = 0;
+			}
 		}
 		System.out.println(result);
 	}
