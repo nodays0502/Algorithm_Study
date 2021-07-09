379319e92878ec2c89df8cd9ddc16edcaa1e5262
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-07-09 22:58:38 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_1753.java
+++ JAVA/BOJ/BOJ_1753.java
@@ -1,69 +1,76 @@
-package Lecture;
+package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
+class Node{
+    int end;
+    int weight;
 
-public class BOJ_1753 {	
-	static ArrayList<int[]> input[];
-	static int V,E,start;
-	static boolean[] visited;
-	static int length[];
-	static void dij() {
-		visited = new boolean[V+1];
-		length = new int[V+1];
-		for(int i = 1 ; i <= V; i++) {
-			length[i] = Integer.MAX_VALUE;
-		}
+    public Node(int end, int weight) {
+        this.end = end;
+        this.weight = weight;
+    }
+}
+public class BOJ_1753 {
+    static int v ,e,k;
+    static List<Node>[] map;
+    static int[] distance;
+    static void dij(){
+        distance = new int[v + 1];
+        boolean[] visited = new boolean[v + 1];
+        Arrays.fill(distance,Integer.MAX_VALUE);
+        distance[k] = 0;
+        for(int i = 0 ; i < v; i++){
+     //       System.out.println(Arrays.toString(distance));
+            int min = Integer.MAX_VALUE;
+            int minIndex = 0;
+            for(int j = 1; j <= v ; j++){
+                if(!visited[j] && min > distance[j]){
+                    min = distance[j];
+                    minIndex = j;
+                }
+            }
+            if(minIndex == 0) return ;
+            visited[minIndex] = true;
 
-		length[start] = 0;
-		for(int t = 0 ; t < V ; t++) {
-			int min = Integer.MAX_VALUE;
-			int minIndex = 0;
-			for(int i = 1 ; i <= V; i++) {
-				if(!visited[i] && min > length[i]) {
-					min = length[i];
-					minIndex = i;
-				}
-			}
-			visited[minIndex] = true;
-			if(min == Integer.MAX_VALUE) break;
-			for(int j = 0 ; j < input[minIndex].size();j++) {
-				int now = input[minIndex].get(j)[0];
-				if(!visited[now] && input[minIndex].get(j)[1] + min < length[now]) {
-					length[now] = input[minIndex].get(j)[1] + min;
-				}
-			}			
-		}
-	}
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
-		start = Integer.parseInt(br.readLine());
-		input = new ArrayList[V+1];
-		for(int i = 0 ; i < V+1 ; i ++) {
-			input[i] = new ArrayList<>();
-		}
-		for(int i = 0 ; i < E ; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			input[a].add(new int[] {b,w});
-		}
-		dij();
-		StringBuilder sb = new StringBuilder();
-		for(int i = 1 ; i <= V; i++) {
-			if(length[i] != Integer.MAX_VALUE)
-				sb.append(length[i]+"\n");
-			//	System.out.println(length[i]);
-			else
-				sb.append("INF\n");
-			//	System.out.println("INF");
-		}
-		System.out.println(sb.toString());
-	}
+            for(int j = 0; j < map[minIndex].size() ; j++){
+                int next = map[minIndex].get(j).end;
+                int weight = map[minIndex].get(j).weight;
+                if(!visited[next] && distance[next] > min + weight){
+                    distance[next] = min + weight;
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        v = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine()," ");
+        k = Integer.parseInt(st.nextToken());
+        map =  new ArrayList[v+1];
+        for(int i = 0 ; i <= v ; i++){
+            map[i] = new ArrayList<>();
+        }
+        for(int i = 0 ; i < e ; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            map[a].add(new Node(b,c));
+      //      map[b].add(new Node(a,c));
+        }
+        dij();
+        for(int i = 1 ; i <= v ; i++){
+            if(distance[i] != Integer.MAX_VALUE)
+                System.out.println(distance[i]);
+            else
+                System.out.println("INF");
+        }
+    }
 }
