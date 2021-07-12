cf7a8128a6f2ad6797dfa9371dcae807485ca0fa
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-07-12 23:19:24 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_11726.java
+++ JAVA/BOJ/BOJ_11726.java
@@ -1,21 +1,18 @@
-package DP;
+package BOJ;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BOJ_11726 {
-	static int dp[];
-	static final int LIMIT = 10007;
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
-		dp = new int[n+1];
-		dp[0] = 1;
-		dp[1] = 1;
-		for(int i = 2 ; i <= n ; i++) {
-			dp[i] = (dp[i-1] + dp[i-2]);
-			dp[i] %= LIMIT;
-		}
-		System.out.println(dp[n]);
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int dp[] = new int[n+3];
+        dp[1] = 1;
+        dp[2] = 2;
+        final int LIMIT = 10007;
+        for(int i = 3; i <=  n; i++){
+            dp[i] = (dp[i-1] + dp[i-2]) % LIMIT;
+        }
+        System.out.println(dp[n]);
+    }
 }
