ac78241e585412a2988afe30f237f621cf1a53f2
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-07-31 12:32:14 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_2003.java
+++ JAVA/BOJ/BOJ_2003.java
@@ -5,36 +5,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_2003 {
-	static int N, M;
-	static int input[];
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine()," ");
-		input = new int[N];
-		for(int i = 0 ; i < N ; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-		}
-		int si = 0;
-		int ei = 0;
-		int sum = 0;
-		int result = 0 ;
-		while(true) {
-			if(sum < M) {
-				if(ei >= N) {
-					break;
-				}
-				sum += input[ei++];
-			}else {
-				if(sum == M) {
-					result++;
-				}
-				sum -= input[si++];
-				if(si >= N) break;
-			}
-		}
-		System.out.println(result);
-	}
+    static int n,m;
+    static int input[];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        input = new int[n];
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i = 0 ; i < n ; i++){
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        int result = 0 ;
+        int si = 0 ;
+        int ei = 0 ;
+        int sum = 0;
+        while(true){
+//            System.out.println("sum:"+sum);
+            if(sum > m){
+                sum -= input[si++];
+            }else if(sum == m){
+                result++;
+                sum -= input[si++];
+            }else{
+                if(ei == n) break;
+                sum += input[ei++];
+            }
+        }
+        System.out.println(result);
+    }
 }
