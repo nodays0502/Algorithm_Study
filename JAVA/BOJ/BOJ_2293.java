b1c6ceaa98dc3210c46d698a85f6eb8e40847590
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-08-01 16:14:21 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_2293.java
+++ JAVA/BOJ/BOJ_2293.java
@@ -2,40 +2,39 @@ package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.function.Function;
 
 public class BOJ_2293 {
-	static int n, k ;
-	static int dp[][];
-	static int input[];
-	static int dfs(int depth, int sum) {
-		if(sum > k) return 0;
-		else if(depth == n && sum == k)return 1;
-		else if(depth >=n && sum != k) return 0;
-		if(dp[depth][sum] != -1) return dp[depth][sum];
-		int result = 0;
-		for(int i = 0; sum + i * input[depth] <= k ; i++) {
-			result += dfs(depth+1, sum + i * input[depth]);
-		}
-		dp[depth][sum] = result;
-//		System.out.println(result);
-		return result;
-	}
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		n = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		dp = new int[n][k+1];
-		input = new int[n];
-		for(int i = 0 ; i < n ; i++) {
-			for(int j = 0 ; j <= k ; j++) {
-				dp[i][j] = -1;
-			}
-		}
-		for(int i = 0 ; i < n ; i++) {
-			input[i] = Integer.parseInt(br.readLine());
-		}
-		System.out.println(dfs(0,0));
-	}
+    static int input[];
+    static int dp[][];
+    static int n , k;
+    static int dfs(int now, int rest){
+        if(rest == 0) return 1;
+        if(now == -1) return 0;
+        if(dp[now][rest] != -1) return dp[now][rest];
+        int result = 0;
+        for(int i = 0; i <= rest / input[now] ; i++){
+            result += dfs(now-1,rest - input[now] * i);
+        }
+        dp[now][rest] = result;
+        return result;
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        Function<String,Integer> stoi = (s) -> Integer.parseInt(s);
+        n = stoi.apply(st.nextToken());
+        k = stoi.apply(st.nextToken());
+        input = new int[n];
+        dp = new int[n][k+1];
+        for(int i = 0 ; i < n ; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            input[i] = stoi.apply(st.nextToken());
+            Arrays.fill(dp[i],-1);
+        }
+        Arrays.sort(input);
+        System.out.println(dfs(n-1,k));
+    }
 }
