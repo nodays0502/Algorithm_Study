382baded4fd457e0353159b641689d0bfc6e5217
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-08-03 22:40:29 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_1717.java
+++ JAVA/BOJ/BOJ_1717.java
@@ -1,48 +1,61 @@
+package BOJ;
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.function.Function;
 
 public class BOJ_1717 {
-	static int n, m ;
-	static int parents[];
-	static void makeSet() {
-		for(int i = 0 ; i <= n ; i++) {
-			parents[i] = i;
-		}
-	}
-	static int findSet(int now) {
-		if(now == parents[now]) return now;
-		else return parents[now]= findSet(parents[now]);
-	}
-	static boolean union(int a, int b) {
-		int aParent = findSet(a);
-		int bParent = findSet(b);
-		if(aParent == bParent) return false;
-		else {
-			parents[aParent] = bParent;
-			return true;
-		}
-	}
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		parents = new int[n+1];
-		makeSet();
-		for(int i = 0 ; i < m ; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			int command = Integer.parseInt(st.nextToken());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			if(command == 0) {
-				union(a,b);
-			}else {
-				if(findSet(a) == findSet(b))
-					System.out.println("YES");
-				else
-					System.out.println("NO");
-			}
-		}
-	}
+
+    static int n, m;
+    static int parent[];
+
+    static int findParent(int a) {
+        if (parent[a] == a) {
+            return a;
+        } else {
+            return parent[a] = findParent(parent[a]);
+        }
+    }
+
+    static boolean setUnion(int a, int b) {
+        int aParent = findParent(a);
+        int bParent = findParent(b);
+        if (aParent == bParent) {
+            return false;
+        } else {
+            parent[bParent] = aParent;
+            return true;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        Function<String, Integer> stoi = Integer::parseInt;
+        n = stoi.apply(st.nextToken());
+        m = stoi.apply(st.nextToken());
+        parent = new int[n + 1];
+        for(int i = 0 ; i <= n ; i++){
+            parent[i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int command = stoi.apply(st.nextToken());
+            int a = stoi.apply(st.nextToken());
+            int b = stoi.apply(st.nextToken());
+            if (command == 0) {
+                setUnion(a, b);
+            } else {
+                int aParent = findParent(a);
+                int bParent = findParent(b);
+                if(aParent == bParent){
+                    System.out.println("YES");
+                }else{
+                    System.out.println("NO");
+                }
+            }
+        }
+    }
 }
