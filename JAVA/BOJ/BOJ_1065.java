7cc6d233c588a902bcf80a0e6a6c48d646ecac50
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-08-05 22:13:24 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_1065.java
+++ JAVA/BOJ/BOJ_1065.java
@@ -1,29 +1,24 @@
 package BOJ;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class BOJ_1065 {
-	static boolean cal(int num) {
-		boolean flag = true;
-		char temp[] = Integer.toString(num).toCharArray();
-		for(int i = 0 ; i < temp.length-2; i++) {
-			if(temp[i]-temp[i+1] == temp[i+1]-temp[i+2]) {
-				;
-			}else {
-				flag = false;
-				break;
-			}
-		}
-		return flag;
-	}
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int result = 0 ;
-		for(int i = 1 ; i <= N ; i++) {
-			if(cal(i)) result++;
-		}
-		System.out.println(result);
-	}
+    static boolean cal(int num){
+        String now = Integer.toString(num);
+        if(now.length() <= 2) return true;
+        for(int i = 0 ; i < now.length() - 2; i++){
+            if(now.charAt(i)-now.charAt(i+1) != now.charAt(i+1)-now.charAt(i+2))
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int result = 0;
+        for(int i = 1 ; i <= n ; i++){
+            if(cal(i))result++;
+        }
+        System.out.println(result);
+    }
 }
