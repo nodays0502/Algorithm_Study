fb7a6be8e55a2c1423247ffa1fa7e04a0237e14a
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-08-08 18:15:28 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_2512.java
+++ JAVA/BOJ/BOJ_2512.java
@@ -1,52 +1,54 @@
-package BinarySearch;
+package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
+import java.util.function.Function;
 
 public class BOJ_2512 {
-	static int input[];
-	static int n;
-	static int m;
-	static int binearySear(int max) {
-		int low = 0;
-		int high = max;
-		int result = 0;
-		while(low <= high) {
-			int mid = (low+high)/2;
-			int sum = cal(mid);
-			if(sum == m) {
-				result = mid;
-				break;
-			}else if(sum < m) {
-				result = Math.max(result,mid);
-				low = mid + 1;
-			}else {
-				high = mid - 1;
-			}
-		}
-		return result;
-	}
-	static int cal(int num) {
-		int sum = 0 ;
-		for(int i = 0 ; i < n ; i++) {
-			sum += Math.min(input[i], num);
-		}
-		return sum;
-	}
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-		n = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine()," ");
-		int max = 0 ;
-		input = new int[n];
-		for(int i = 0 ; i < n ; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-			max = Math.max(max, input[i]);
-		}
-		st = new StringTokenizer(br.readLine()," ");
-		m = Integer.parseInt(st.nextToken());
-		System.out.println(binearySear(max));
-	}
+    static int n,m;
+    static int input[];
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine()," ");
+        Function<String,Integer> stoi = Integer::parseInt;
+        n = stoi.apply(st.nextToken());
+        st= new StringTokenizer(br.readLine()," ");
+        input = new int[n];
+        int sum = 0 ;
+        int max = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            input[i] = stoi.apply(st.nextToken());
+            sum += input[i];
+            max = Math.max(input[i],max);
+        }
+        st= new StringTokenizer(br.readLine()," ");
+        m = stoi.apply(st.nextToken());
+        if(sum <= m){
+            System.out.println(max);
+            return ;
+        }
+        int result = 0;
+        int start = 0 ;
+        int end = 1000000000;
+        while(start <= end){
+            int mid = (start + end)/2;
+            if(cal(mid)){
+                result = Math.max(result,mid);
+                start = mid + 1;
+            }else{
+                end = mid - 1;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private static boolean cal(int mid) {
+        int sum = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            sum += Math.min(input[i],mid);
+        }
+        if(sum <= m)return true;
+        else return false;
+    }
 }
