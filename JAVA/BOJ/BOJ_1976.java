f32c226f3b1f67dbe34ae8dfe259fc685465d08b
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-08-12 22:24:19 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_1976.java
+++ JAVA/BOJ/BOJ_1976.java
@@ -1,63 +1,58 @@
+package BOJ;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_1976 {
-	static int parents[];
-	static void makeSet() {
-		for(int i = 0 ; i <= n ; i++) {
-			parents[i] = i;
-		}
-	}
-	static int findSet(int now) {
-		if(now == parents[now]) return now;
-		else return parents[now] = findSet(parents[now]);
-	}
-	static boolean union(int a, int b) {
-		int aParent = findSet(a);
-		int bParent = findSet(b);
-		if(aParent == bParent) return true;
-		else {
-			parents[aParent] = bParent;
-			return false;
-		}
-	}
-	static int n;
-	static int m;
-	static int input[];
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		n = Integer.parseInt(br.readLine());
-		m = Integer.parseInt(br.readLine());
-		input = new int[m];
-		parents = new int[n+1];
-		makeSet();
-		for(int i = 1 ; i <= n ; i++) {
-			st = new StringTokenizer(br.readLine()," ");
-			for(int j = 1 ; j <= n ; j++) {
-				int bool = Integer.parseInt(st.nextToken());
-				if(bool == 1) {
-					union(i,j);
-				}
-			}
-		}	
-		st = new StringTokenizer(br.readLine()," ");
-		for(int i = 0 ; i < m ; i++) {
-			input[i] = Integer.parseInt(st.nextToken());
-		}
-		boolean flag = true;
-		for(int i = 1 ; i < m ; i++) {
-			int aParent = findSet(input[i-1]);
-			int bParent = findSet(input[i]);
-			if(aParent != bParent) {
-				flag = false;
-				break;
-			}
-		}
-		if(flag)
-			System.out.println("YES");
-		else
-			System.out.println("NO");
-	}
+    static int n,m;
+    static int map[][];
+    static void floyd(){
+        for(int k = 1 ; k <= n ; k++){
+            for(int i = 1 ; i <= n ; i++){
+                for(int j = 1 ; j <= n ; j++){
+                    if(map[i][k] == 1 && map[k][j] == 1){
+                        map[i][j] = 1;
+                    }
+                }
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine()," ");
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n+1][n+1];
+        for(int i = 1 ; i <= n ; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            for(int j = 1 ; j <= n ; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(i == j)
+                    map[i][j] = 1;
+            }
+        }
+        floyd();
+        st = new StringTokenizer(br.readLine()," ");
+        int input[] = new int[m];
+        for(int i = 0 ; i < m ; i++){
+            input[i] = Integer.parseInt(st.nextToken());
+        }
+        boolean result = true;
+        for(int i = 0 ; i < m-1 ; i++){
+            int a = input[i];
+            int b = input[i+1];
+            if(map[a][b] == 0){
+                result = false;
+                break;
+            }
+        }
+//        System.out.println(Arrays.deepToString(map));
+        if(result){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
 }
