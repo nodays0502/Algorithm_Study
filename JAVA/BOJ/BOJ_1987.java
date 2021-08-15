e8eea0361a458428d04066dd85f3a676c20d84c6
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-08-15 12:35:34 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_1987.java
+++ JAVA/BOJ/BOJ_1987.java
@@ -1,43 +1,44 @@
-package HW;
+package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.function.Function;
 
 public class BOJ_1987 {
-	static int N , M;
-	static char map[][];
-	static boolean use[] = new boolean[26];
-//	static int result= 0;
-	static int dy[] = {0,1,0,-1};
-	static int dx[] = {1,0,-1,0};
-	static int dfs(int y , int x) {
-		int result = 0;
-		for(int i = 0 ; i < 4 ; i++) {
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if(nx>= 0 && nx < M && ny >= 0 && ny < N && !use[map[ny][nx] - 'A']) {
-				use[map[ny][nx] - 'A'] = true;
-				result = Math.max(result, dfs(ny,nx));
-				use[map[ny][nx] - 'A'] = false;
-			}
-		}
-		return result + 1;
-	//		System.out.println(result);
-	}
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " " );
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new char[N][];
-		for(int i = 0 ; i < N ; i++) {
-			map[i] = br.readLine().toCharArray();
-		}
+    static int n,m;
+    static char map[][];
+    static boolean count[] = new boolean[26];
+    static int dy[] = {0,1,0,-1};
+    static int dx[] = {1,0,-1,0};
+    static int cal(int y, int x){
+        int result = 0;
+        count[map[y][x]-'A'] = true;
+        for(int i = 0 ; i < 4 ; i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if(nx >= 0 && nx < m && ny >= 0 && ny < n && !count[map[ny][nx]-'A'] ){
+                result = Math.max(cal(ny,nx),result);
+            }
+        }
+        count[map[y][x]-'A'] = false;
+        return result + 1 ;
+    }
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        Function<String,Integer> stoi = Integer::parseInt;
+        n = stoi.apply(st.nextToken());
+        m = stoi.apply(st.nextToken());
+        map = new char[n][m];
+        for(int i = 0 ; i < n ;  i++){
+            String command = br.readLine();
+            for(int j = 0 ; j < m ; j++){
+                map[i][j] = command.charAt(j);
+            }
+        }
+        int result = cal(0,0);
+        System.out.println(result);
+    }
 
-		use[map[0][0] - 'A'] = true;
-		System.out.println(dfs(0,0));
-		
-	}
 }
