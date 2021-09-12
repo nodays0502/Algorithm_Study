fe5d8aeff4670942c77230f460d00f3a10b93b46
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-09-12 23:53:57 +0900

    Add files via upload

---
+++ JAVA/BOJ/BOJ_11718.java
@@ -0,0 +1,16 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_11718 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String command = br.readLine();
+        while(command != null){
+            System.out.println(command);
+            command = br.readLine();
+        }
+    }
+}
