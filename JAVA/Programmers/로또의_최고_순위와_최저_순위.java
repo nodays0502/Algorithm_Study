3ba7f0ac7b5be0b2e0ec091db6fed16ce471e521
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-10-17 18:07:23 +0900

    Update 로또의_최고_순위와_최저_순위.java

--- JAVA/Programmers/로또의_최고_순위와_최저_순위.java
+++ JAVA/Programmers/로또의_최고_순위와_최저_순위.java
@@ -1,37 +1,39 @@
-package self;
-
-import java.util.HashSet;
-import java.util.Set;
-
-public class 로또의_최고_순위와_최저_순위 {
-    static int cal(int sum){
-        int result = 6;
-        if(sum == 6) result = 1;
-        else if(sum == 5) result =2;
-        else if(sum == 4) result = 3;
-        else if(sum == 3) result = 4;
-        else if(sum == 2) result = 5;
-        
-        return result;
-    }
+import java.util.*;
+class Solution {
+    static Set<Integer> set = new HashSet<>();
     public int[] solution(int[] lottos, int[] win_nums) {
-        int[] answer = new int[2];
+        int[] answer = new int[] {0,0};
+        for(int num : win_nums){
+            set.add(num);
+        }
         int cnt = 0;
         int zeroCnt = 0;
-        Set<Integer> s = new HashSet<>();
-        for(int i = 0 ; i < win_nums.length; i++)
-            s.add(win_nums[i]);
-        for(int i = 0 ; i < lottos.length ; i++){
-            if(lottos[i] == 0) zeroCnt++;
-            else{
-                if(!s.add(lottos[i]))
-                    cnt++;
+        for(int lotto : lottos){
+            if(lotto == 0){
+                zeroCnt++;
+            }else if(set.contains(lotto)){
+                cnt++;
             }
         }
-        int sum = cnt + zeroCnt;
-        answer[0] = cal(sum);
+        answer[0] = cal(cnt+zeroCnt);
         answer[1] = cal(cnt);
         return answer;
     }
-
+    private int cal(int cnt){
+        int result = 0;
+        if(cnt == 6){
+            result = 1;
+        }else if(cnt == 5){
+            result = 2;
+        }else if(cnt == 4){
+            result = 3;
+        }else if(cnt == 3){
+            result = 4;
+        }else if(cnt == 2){
+            result = 5;
+        }else{
+            result = 6;
+        }
+        return result;
+    }
 }
