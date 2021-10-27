29699a10f4236baf8fd8204c66aa3a6adf689ed3
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-10-27 21:27:33 +0900

    Update 신규_아이디_추천.java

--- JAVA/Programmers/신규_아이디_추천.java
+++ JAVA/Programmers/신규_아이디_추천.java
@@ -51,3 +51,55 @@ class Solution {
         return answer; 
     }
 }
+
+/* 2
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder(new_id.toLowerCase()); // 1단계
+        for(int i = 0 ; i < sb.length() ; i++){ // 2단계
+            char now = sb.charAt(i);
+            if((now >= 'a' && now <= 'z') || (now >= '0' && now <= '9') || now == '-'
+              || now == '_' || now == '.'){
+                
+            }else{
+                sb.deleteCharAt(i);
+                i--;
+            }
+        }
+        boolean flag = false;
+        for(int i = 0 ; i < sb.length() ; i++){ // 3단계
+            char now = sb.charAt(i);
+            if(now == '.' && flag){
+                sb.deleteCharAt(i);
+                i--;
+            }else if(now == '.'){
+                flag = true;
+            }else{
+                flag = false;
+            }
+        }
+        if(sb.length() > 0 && sb.charAt(0) == '.'){ // 4단계
+            sb.deleteCharAt(0);
+        }
+        if(sb.length() > 0 && sb.charAt(sb.length() -1) == '.'){
+            sb.setLength(sb.length() -1);
+        }
+        if(sb.length() == 0){ // 5단계
+            sb.append('a');
+        }
+        if(sb.length() >= 16){
+            sb.setLength(15);
+        }
+        if(sb.length() > 0 && sb.charAt(sb.length() -1) == '.'){ // 6단계
+            sb.setLength(sb.length() -1);
+        }
+        while(sb.length() <= 2){
+            sb.append(sb.charAt(sb.length()-1));
+        }
+        answer= sb.toString();
+        return answer;
+    }
+}
+
+*/
