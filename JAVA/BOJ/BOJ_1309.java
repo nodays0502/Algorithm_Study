48e3048b3d1d2aa02ad536d1f93ce713ff5bc6fb
nodays0502 <76464235+nodays0502@users.noreply.github.com>
2021-12-14 12:27:19 +0900

    Add files via upload

--- JAVA/BOJ/BOJ_1309.java
+++ JAVA/BOJ/BOJ_1309.java
@@ -2,19 +2,24 @@ package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class BOJ_1309 {
     static int dp[][];
     static int n;
-
+    static final int LIMIT = 9901;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        dp = new int[3][n];
-        Arrays.fill(dp,1);
-        for(int i = 0 ;i < n; i++){
-            dp[0][i] = dp[0][i-1] + dp[1][i-1] + dp[2][i-1];
+        dp = new int[n+5][3];
+        dp[1][0] = 1;
+        dp[1][1] = 1;
+        dp[1][2] = 1;
+        for(int i = 2 ;i <= n; i++){
+            dp[i][0] = (dp[i-1][0] + dp[i-1][1] + dp[i-1][2]) % LIMIT;
+            dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % LIMIT;
+            dp[i][2] = (dp[i-1][0] + dp[i-1][2]) % LIMIT;
         }
+        int result = (dp[n][0] + dp[n][1] + dp[n][2]) % LIMIT;
+        System.out.println(result);
     }
 }
